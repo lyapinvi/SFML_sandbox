2f973db655598618ac5c7c7a4165598d074c6f8d
lyapinvi <lyapinvi@ya.ru>
2018-03-20 21:58:05 +0300

    add Snake class declaration

--- snake/Snake.h
+++ snake/Snake.h
@@ -9,3 +9,46 @@ using SnakeContainer = std::vector<SnakeSegment>;
 
 enum class Direction {None, Up, Down, Left, Right};
 
+class Snake {
+public:
+	Snake(int blocksize);
+	~Snake();
+
+	// Helper methods
+	void SetDirection(Direction dir);
+	Direction GetDirection() const;
+	int GetSpeed();
+	sf::Vector2i GetPosition();
+	int GetLives();
+	int GetScore();
+	void IncreaseScore();
+	bool HasLost();
+
+	// Losing
+	void Lose();
+	void ToggleLost();
+
+	// Growth
+	void Extend();
+
+	//Reset
+	void Reset();
+
+	void Move();
+	void Tick();
+	void Cut(int segments);
+	void Render(sf::RenderWindow& window);
+
+private:
+	void CheckCollision();
+
+private:
+	SnakeContainer m_snakeBody;
+	int m_size;
+	Direction m_dir;
+	int m_speed;
+	int m_lives;
+	int m_score;
+	bool m_lost;
+	sf::RectangleShape m_bodyRect;
+};
